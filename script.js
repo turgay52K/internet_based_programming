defabc5d264e15bb62e79cefef14fc462f78b8cc
Turgay Karademir <turgay.k052@gmail.com>
2023-06-06 20:44:45 +0300

    v1.5

---
+++ script.js
@@ -0,0 +1,9 @@
+const arrows=document.querySelectorAll(".arrow");
+const movieLists=document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i)=>{
+    arrow.addEventListener( "click",function(){
+        movieLists[i].style.transform=`translateX(${movieLists[i].computedStyleMap.get("transform")[0].x.value-300}px)`
+      });
+      
+})
